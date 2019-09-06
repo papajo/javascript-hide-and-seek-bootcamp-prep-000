44060c9c9fe991cad36e9a3e666d540ce59a1243
PJ Joshi <joshipv2@gmail.com>
2019-09-06 22:50:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const elst = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < elst.length; i++) {
+    //debugger
+    for (let j = 0; j < elst[i].children.length; j++) {
+        var tempLi = elst[i].children
+        tempLi[j].innerHTML = (parseInt(tempLi[j].innerHTML) + n).toString();
+    }
+  }
+}
+
+function deepestChild() {
+  /* Define a function deepestChild() that pulls out 
+  the most deeply nested child element 
+  from div#grand-node. */
+  const temArray = document.queryElementById('grand-node')
+  debugger
+}
